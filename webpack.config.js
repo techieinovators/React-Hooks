b98c94da772161d25b5e909b2ae7b104fb8b6f0f
techieinovators <61652126+techieinovators@users.noreply.github.com>
2021-09-26 10:06:43 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
